ac65555a330bc4a3b0ebad42055740739589586a
kwang19 <kwang19@uchicago.edu>
2017-02-14 17:35:10 -0500

    ok for update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  # this is a route for this project
   get("/permanent_programs", { :controller => "programs", :action => "permanent_programs" })
 
   get("/conditionals", { :controller => "programs", :action => "conditionals" })
